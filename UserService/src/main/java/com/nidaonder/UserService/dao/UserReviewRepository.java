b12fbd561da28ec22a2609e3dcce424146544a5a
nidaonder <nidaaonder@gmail.com>
2024-03-08 02:17:22 +0300

    added repository annotation.

--- UserService/src/main/java/com/nidaonder/UserService/dao/UserReviewRepository.java
+++ UserService/src/main/java/com/nidaonder/UserService/dao/UserReviewRepository.java
@@ -2,6 +2,8 @@ package com.nidaonder.UserService.dao;
 
 import com.nidaonder.UserService.entity.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
 }
