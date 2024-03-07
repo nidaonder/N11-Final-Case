7966e5656428853ad3a1d053a1d37b8a7a083688
nidaonder <nidaaonder@gmail.com>
2024-03-08 02:16:23 +0300

    added find user by email.

--- UserService/src/main/java/com/nidaonder/UserService/dao/UserRepository.java
+++ UserService/src/main/java/com/nidaonder/UserService/dao/UserRepository.java
@@ -2,6 +2,11 @@ package com.nidaonder.UserService.dao;
 
 import com.nidaonder.UserService.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
